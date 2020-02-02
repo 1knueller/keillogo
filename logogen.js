2b5f3f75a13a038db0be28ce2db2a191690009d1
keli <photosynthese@gmx.at>
2020-02-02 13:34:48 +0100

    implemented point locking and min area

--- logogen.js
+++ logogen.js
@@ -1,5 +1,10 @@
 var s = Snap("#paper");
 
+var ltcbox = document.getElementById("locktop");
+var lrcbox = document.getElementById("lockright");
+var llcbox = document.getElementById("lockleft");
+var afbox = document.getElementById("areafactor");
+
 function generate() {
     paint(192, 108);
 }
@@ -10,40 +15,60 @@ function paint(w, h) {
     // debug rect
     s.rect(0, 0, w, h).attr({ fill: "#EEEEEE" });
 
+    // TEXT
     var text = s.text(0, 0, "KEIL");
     text.attr({ "font-size": 18 });
-
     var textbb = text.getBBox();
-    // text padding
+    // TEXT padding
     var tpadl = 0.05;
     var tpadb = 0.05;
     text.attr({ x: w - textbb.width - w * tpadl, y: h - w * tpadb });
 
     var t = getTriangleWithMinArea(w, h);
     var triangle = s.polyline(t.Ax, t.Ay, t.Bx, t.By, t.Cx, t.Cy);
-    textbb = text.getBBox();
 
-    var x = textbb.x;
-    var y = textbb.y;
-    document.getElementById("dbg").innerHTML = x;
+    //textbb = text.getBBox();
+
+    //var x = textbb.x;
+    //var y = textbb.y;
+    //document.getElementById("dbg").innerHTML = x;
 }
 
 function getTriangleInRect(w, h) {
+    var top = 0;
+    if (!(ltcbox && ltcbox.checked))
+        top = Math.random() * h;
+
+    var right = w;
+    if (!(lrcbox && lrcbox.checked))
+        right = Math.random() * w;
+
+    var left = 0;
+    if (!(llcbox && llcbox.checked))
+        left = Math.random() * w;
+
     return {
         Ax: Math.random() * w,
-        Ay: Math.random() * h,
-        Bx: Math.random() * w,
+        Ay: top,
+        Bx: right,
         By: Math.random() * h,
-        Cx: Math.random() * w,
+        Cx: left,
         Cy: Math.random() * h
     };
 }
 
 function getTriangleWithMinArea(w, h) {
     var t;
+
+    var minarea = w * h * 0.15;
+    if (afbox)
+        minarea = w * h * afbox.value;
+
+    var i = 100;
     do {
         t = getTriangleInRect(w, h);
-    } while (GetTriangleArea(t) < 100);
+        i--;
+    } while (GetTriangleArea(t) < minarea || i > 0);
 
     return t;
 }
